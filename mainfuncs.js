017796b2b4e6bd4bd8cca2f03c241082183cdf6d
starbuck753 <starbuck753@gmail.com>
2018-10-25 13:02:51 -0300

    Add about section

--- mainfuncs.js
+++ mainfuncs.js
@@ -12,34 +12,12 @@ function lastModified(){
 
 }
 
-function writepics(homepage) {
+function abouticon() {
 
-var str;
-var lista = new Array();
-
-for (n=0; n<= 76; n++){
-	lista[n] = n+1;
-}
-for (n=0; n<=76; n++){
-	var rand1 = Math.floor((Math.random()*77));
-	val = lista[n];
-	lista[n] = lista[rand1];
-	lista[rand1] = val;
-}
-
-str = "<section class=\"slider\"><div class=\"flexslider carousel\"><ul class=\"slides\">";
-
-for (n=0; n<=76; n++){
-	if (homepage==1)
-	{
-		str = str + "<li><img src=\"images/icon_" + lista[n] + ".png\" /></li>\n";
-	}else
-	{
-		str = str + "<li><img src=\"../images/icon_" + lista[n] + ".png\" /></li>\n";
-	}
-}
-
-str = str + "</ul></div></section>";
-
-document.write(str);
+	var str;
+	
+	var rand1 = Math.floor((Math.random()*7)+1);
+	str = "<img src=\"images/icon_" + rand1 + ".png\" />";
+	
+	document.write(str);
 }
